9ffacb0c1d5aba42455890a3d4d6b4f1c84ef30e
Ben Tu <ben.tu16@imperial.ac.uk>
2023-03-29 12:58:56 +0100

    Add new test functions

--- scalarize/test_functions/__init__.py
+++ scalarize/test_functions/__init__.py
@@ -1,8 +1,21 @@
 #!/usr/bin/env python3
 
-from scalarize.test_functions.multi_objective import MarineDesign, RocketInjector
+from scalarize.test_functions.multi_objective import (
+    CabDesign,
+    FourBarTrussDesign,
+    MarineDesign,
+    ResourcePlanning,
+    RocketInjector,
+    TwoBarTrussDesign,
+    WeldedBeam,
+)
 
 __all__ = [
+    "CabDesign",
+    "FourBarTrussDesign",
     "MarineDesign",
+    "ResourcePlanning",
     "RocketInjector",
+    "TwoBarTrussDesign",
+    "WeldedBeam",
 ]
