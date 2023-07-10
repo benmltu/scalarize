a29bdb6d90c4b7e8813ffc792627e8098c0af984
Ben Tu <ben.tu16@imperial.ac.uk>
2023-07-10 17:04:44 +0100

    Add robust methods

--- scalarize/acquisition/__init__.py
+++ scalarize/acquisition/__init__.py
@@ -2,8 +2,18 @@
 
 from scalarize.acquisition.analytic import Uncertainty
 from scalarize.acquisition.monte_carlo import qNoisyExpectedImprovement
+from scalarize.acquisition.robust_objectives import (
+    ChiSquare,
+    Entropic,
+    MCVaR,
+    TotalVariation,
+)
 
 __all__ = [
+    "ChiSquare",
+    "Entropic",
+    "MCVaR",
+    "TotalVariation",
     "qNoisyExpectedImprovement",
     "Uncertainty",
 ]
